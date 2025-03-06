fa28e1078026430ff1ddace743589c1a84798bff
saif el_dein tarek <saifel.dein01010@gmail.com>
2025-03-06 14:41:25 +0200

    Add Class of Menu screen design

---
+++ DesignOfMenu.cs
@@ -0,0 +1,70 @@
+﻿
+using System;
+using CalculatorProject.utility;
+
+namespace MathOperation
+{
+    class DesignOfMenu
+    {
+        private int numberOfChooise = 0;
+        private void HeaderScreen()
+        {
+
+            Console.WriteLine("\t\t\t\t < - - Welcome to math operation - - >");
+
+        }
+
+        private void OptionsScreen()
+        {
+
+            Console.WriteLine("\t [1] Addition       (+)");
+            Console.WriteLine("\t [2] Subtraction    (-)");
+            Console.WriteLine("\t [3] Multiplication (*)");
+            Console.WriteLine("\t [4] Division       (/)");
+            Console.WriteLine("\t [5] Show previous result.");
+
+            Console.WriteLine("\t------------------------------");
+
+            numberOfChooise = ReadInput.ReadNumberBetweenRange("\tEnter your chooises: ", 1, 5);
+
+        }
+
+        private void PerformOption()
+        {
+
+            switch(numberOfChooise)
+            {
+
+                case 1:
+                    break;
+
+                case 2:
+                    break;
+
+                case 3:
+                    break;
+
+                case 4:
+                    break;
+
+                case 5:
+                    break;
+
+            }
+
+        }
+
+
+
+        public void ShowMenuScreen()
+        {
+
+            HeaderScreen();
+            OptionsScreen();
+            PerformOption();
+
+        }
+
+    }
+
+}
---
+++ ReadInput.cs
@@ -0,0 +1,119 @@
+﻿
+
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace CalculatorProject.utility
+{
+    
+    class ReadInput
+    {
+
+        static public string ReadText(string message)
+        {
+
+            string text = "";
+
+            Console.Write(message);
+            text = Console.ReadLine();
+
+            return text;
+
+        }
+
+        static public string ReadText()
+        {
+
+            string text = "";
+            text = Console.ReadLine();
+
+            return text;
+
+        }
+
+        public static dynamic ReadNumber()
+        {
+
+            string stringNumber = Console.ReadLine();
+            dynamic number = 0;
+
+            while (!(int.TryParse(stringNumber, out int num1)) && !(double.TryParse(stringNumber, out double num2)))
+            {
+
+                Console.Write($"this is not vaild \"{stringNumber}\" number. Please, Enter vaild number > ");
+                stringNumber = Console.ReadLine();
+
+            }
+
+            if((int.TryParse(stringNumber, out int i)))
+            {
+                number = Convert.ToInt32(stringNumber);
+            }
+            else
+            {
+                number = Convert.ToDouble(stringNumber);
+            }
+
+            return number;
+
+        }
+
+        public static dynamic ReadNumber(string message)
+        {
+
+            Console.Write(message);
+            return ReadNumber();
+
+        }
+
+        static public bool IsNumber(string digit)
+        {
+            dynamic number = 0;
+            if(!(int.TryParse(digit, out int numb1)) && !(double.TryParse(digit, out double num2)))
+            {
+                return false;
+            }
+
+            return true;
+
+        }
+
+        static public dynamic ReadNumberBetweenRange(string message, dynamic from, dynamic to)
+        {
+
+            dynamic number = ReadNumber(message);
+
+            while(number < from || number > to)
+            {
+
+                Console.WriteLine("Number is out of range");
+                number = ReadNumber(message);
+
+            }
+
+            return number;
+        }
+
+
+        static public char ReadLetter()
+        {
+
+            char letter = ' ';
+
+            letter = Convert.ToChar(Console.ReadLine());
+
+            return letter;
+
+        }
+
+        static public char ReadLetter(string message)
+        {
+
+            Console.Write(message);
+            return ReadLetter();
+
+        }
+
+
+    }
+
+}
